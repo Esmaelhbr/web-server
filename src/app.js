df78d504ae8135e2e332830d0d2d4df789d763e7
Esmaelhbr <esmael7@live.com>
2021-10-27 13:26:11 -0400

    solved untracked files, add geolocation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 
 const app = express();
@@ -50,15 +52,28 @@ app.get("/weather", (req,res) => {
 	let address= req.query.adress;
 	if(!req.query.address){
 		return res.send({
-			error :"you must provide an address"}
-			)
+			error :"you must provide an address"
+		})
 	}
-	res.send({
-		
-		location: "Boston",
-		forecast:"Rainning",
-		address: req.query.address
-	});
+
+	geocode(req.query.address,(error, {latitude, longitude, location} = {}) => {
+		if(error){
+			return res.send({error})
+		}
+		forecast(latitude,longitude,(error, forecastData) => {
+			if(error){
+				return res.send(error)
+			}
+			res.send({
+				forecast: forecastData,
+				location,
+				address: req.query.address
+
+			})
+
+		})
+
+	})
 })
 
 app.get("/products", (req,res) => {
