ebbb0809c57997ef738465eded741bbee24d9054
Esmaelhbr <esmael7@live.com>
2021-10-27 14:09:29 -0400

    set up the fetch function to get weather from an end point

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log("client side javascript")
+
+fetch('http://localhost:3000/weather?address=boston').then((response) =>{
+	response.json().then((data) =>{
+		if(data.error){
+			console.log(data.error)
+		}else{
+			console.log(data.location);
+			console.log(data.forecast)
+		}
+		
+	})
+})
